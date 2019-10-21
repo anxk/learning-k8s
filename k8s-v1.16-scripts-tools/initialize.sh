694ce882c5150f25eb2e37932edef58f9ed4fc4a
anxk <anxiaokanghit@163.com>
2019-10-21 23:45:56 +0800

    添加k8s初始化配置和kubelet,kubectl,kubeadm的安装

---
+++ k8s-v1.16-scripts-tools/initialize.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -ex
+
+# Set kubernetes.repo
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+
+# Set SELinux in permissive mode
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# Set iptables config
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+# Set swappiness
+swapoff -a
+cat <<EOF >> /etc/sysctl.d/k8s.conf
+vm.swappiness=0
+EOF
+
+# Reload sysctl config
+sysctl --system
+
+# Install kubelet kubeadm kubectl
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+# restart kubelet
+systemctl daemon-reload
+systemctl restart kubelet
+systemctl enable kubelet
